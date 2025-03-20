2d71b34b7a7399e80832748cf9a49c17be741a2a
Kiruba karan A <kirubakaran@trdfin.in>
2025-03-20 12:02:13 +0530

    NK CRAFTINGS

---
+++ elsapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ElsappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'elsapp'
---
+++ elsapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+# Create your views here.
+
+
+def index(request):
+    return render(request,'index.html')
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
